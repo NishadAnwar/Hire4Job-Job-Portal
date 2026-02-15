4f95bedb5cfef83ce0e3dd313db81b8b8b908765
Md Nishad Anwar <120729890+NishadAnwar@users.noreply.github.com>
2026-02-16 00:16:48 +0530

    Update login API endpoint to production URL

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:5000/api/v1/user/login",
+        "https://hire4job-job-portal-backend.onrender.com/api/v1/user/login",
         { email, password, role },
         {
           headers: {
