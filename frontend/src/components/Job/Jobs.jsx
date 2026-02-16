e511662ae5f6398a8985abf0367541475877aa05
Md Nishad Anwar <120729890+NishadAnwar@users.noreply.github.com>
2026-02-16 19:56:30 +0530

    Fix template literal syntax in API call

--- frontend/src/components/Job/Jobs.jsx
+++ frontend/src/components/Job/Jobs.jsx
@@ -10,7 +10,7 @@ const Jobs = () => {
   useEffect(() => {
     try {
       axios
-        .get("${process.env.REACT_APP_BACKEND_URL}/api/v1/job/getall", {
+        .get(`${process.env.REACT_APP_BACKEND_URL}/api/v1/job/getall`, {
           withCredentials: true,
         })
         .then((res) => {
