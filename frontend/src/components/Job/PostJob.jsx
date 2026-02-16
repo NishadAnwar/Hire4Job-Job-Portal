b2c10a6ad394cdd87d0407a68e04e4b0e7b793da
Md Nishad Anwar <120729890+NishadAnwar@users.noreply.github.com>
2026-02-16 19:36:03 +0530

    Change API endpoint to use environment variable
    Updated the job posting API endpoint to use environment variable.

--- frontend/src/components/Job/PostJob.jsx
+++ frontend/src/components/Job/PostJob.jsx
@@ -31,7 +31,7 @@ const PostJob = () => {
     }
     await axios
       .post(
-        "https://hire4job-job-portal-backend.onrender.com/api/v1/job/post",
+        "${process.env.REACT_APP_BACKEND_URL}/api/v1/job/post",
         fixedSalary.length >= 4
           ? {
               title,
