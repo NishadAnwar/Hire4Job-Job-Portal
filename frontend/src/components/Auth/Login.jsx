6c22c3561adab17cd5a2c3e50134ddf3ea162df6
Md Nishad Anwar <120729890+NishadAnwar@users.noreply.github.com>
2026-02-16 19:33:15 +0530

    Update login API URL to use environment variable

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://hire4job-job-portal-backend.onrender.com/api/v1/user/login",
+        "${process.env.REACT_APP_BACKEND_URL}/api/v1/user/login",
         { email, password, role },
         {
           headers: {
